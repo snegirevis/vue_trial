522692e81eed12b236e016271e0e81e49c44027b
snegirevis <sngraindrops@gmail.com>
2023-02-12 22:21:33 +0600

    2nd commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,94 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+
+<div class="app">
+  <form @submit.prevent>
+    <h4>Введите данные о Вашем котике</h4>
+    <input v-bind:value="name" @input="name= $event.target.value" class="input" type="text" placeholder="Имя котика">
+    <input v-bind:value="age" @input="age= $event.target.value" class="input" type="text" placeholder="Возраст котика">
+    <button class="btn" @click="createCat">Добавить</button>
+  </form>
+
+  <div class="cats" v-for="cat in cats">
+  <div><strong>Имя:</strong>{{ cat.name}}</div>
+  <div><strong>Возраст:</strong>{{ cat.age}}</div>
+  </div>
+</div>
+
 </template>
 
+<script>
+
+export default{
+    data(){
+      return {
+        cats:[
+          {id:1, name:'Barsik', age: 2},
+          {id:2, name:'Murzik', age: 3},
+          {id:3, name:'Julia', age: 4},
+        ],
+
+        name: '',
+        age: '',
+       
+      }
+    },
+      methods:{
+
+      createCat(){
+       const newCat={
+        id:Date.now(),
+        name:this.name,
+        age:this.age,
+       }
+        this.cats.push(newCat);
+        this.name='';
+        this.age='';
+      }
+     
+      
+
+      }
+    }
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+.app{
+padding: 20px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+form{
+  display: flex;
+  flex-direction: column;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.cats{
+  padding:15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+
+.input{
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn{
+ margin-top: 15px;
+ align-self: center;
+ padding: 10px 15px;
+ background: none;
+ color: teal;
+ border-radius: 20px;
+ border: 1px solid;
+
 }
 </style>
